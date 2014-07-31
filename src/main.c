81f6face494fe4d0fcb1bf44c772c49e7c22193f
rbelouin <rodolphe.belouin@gmail.com>
2014-08-01 01:47:19 +0200

    Generate a list of notes.

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <CoreServices/CoreServices.h>
+#include <CoreMIDI/CoreMIDI.h>
+
+#include "midi.h"
+
+int main(int argc, const char **argv) {
+  listenMIDIEndpoints();
+
+  return 0;
+}
---
+++ src/midi.c
@@ -0,0 +1,121 @@
+#include <CoreServices/CoreServices.h>
+#include <CoreMIDI/CoreMIDI.h>
+
+#include "midi.h"
+
+MIDICommand packet2command(MIDIPacket packet) {
+  MIDICommand command = {0, 0, 0};
+
+  if (packet.length) {
+    command.code = packet.data[0];
+    command.note = packet.data[1];
+    command.velocity = packet.data[2];
+  }
+
+  return command;
+}
+
+void printNotes(State* state) {
+  Note* iterator = state->notes.note;
+
+  if (iterator != NULL) {
+    printf("[");
+
+    for (int i = 0; i < state->notes.count - 1; i++) {
+      printf("%s%s, ", iterator->name, iterator->octave);
+      iterator = iterator->next;
+    }
+
+    printf("%s%s]\n", iterator->name, iterator->octave);
+  }
+
+  state->running = 0;
+}
+
+void handleCommand(MIDICommand command, State* state) {
+  static const char *noteNames[12] = {"c", "db", "d", "eb", "e", "f", "gb", "g", "ab", "a", "bb", "b"};
+  static const char *octaveNames[11] = {"_____", "____", "___", "__", "_", "", "'", "''", "'''", "''''", "'''''"};
+
+  int note;
+  int octave;
+
+  /* If the command is related to the sustain pedal */
+  if (state->running && command.code == 176) {
+    state->sustain = command.velocity == 0 ? 0 : 1;
+
+    /* Start recording if the user presses it */
+    if (state->sustain) {
+      fprintf(stderr, "Start recording…\n");
+    }
+    else {
+      printNotes(state);
+    }
+  }
+  /* Else if we are recording, save the pressed note */
+  else if (state->running && state->sustain && command.code == 144) {
+    state->notes.count++;
+
+    note = command.note % 12;
+    octave = command.note / 12 % 11;
+
+    state->lastNote = malloc(sizeof(Note));
+    state->lastNote->name = (char*) noteNames[note];
+    state->lastNote->octave = (char*) octaveNames[octave];
+    state->lastNote->next = NULL;
+
+    if (state->currentNote != NULL) {
+      state->currentNote->next = state->lastNote;
+    }
+    else {
+      state->notes.note = state->lastNote;
+    }
+    state->currentNote = state->lastNote;
+  }
+}
+
+void readPackets(const MIDIPacketList* list, void* readProcRefCon, void* ref) {
+  MIDIPacket* packet;
+
+  if (list->numPackets > 0) {
+    packet = (MIDIPacket*) &list->packet[0];
+
+    for (int i = 0; i < list->numPackets; i++) {
+      handleCommand(packet2command(*packet), (State *) ref);
+    }
+  }
+}
+
+void listenMIDIEndpoints() {
+  /* Initialize state */
+  State state = {1, 0, {0, NULL}, NULL, NULL};
+
+  /* Get the MIDI endpoints */
+  MIDIEndpointRef src = MIDIGetSource(0);
+
+  MIDIClientRef client;
+  MIDIPortRef port;
+
+  CFStringRef name = NULL;
+  //name of the endpoint
+  MIDIUniqueID sourceID = 0;
+  //source ID of the endpoint
+
+  /* Get the properties of the given endpoint */
+  MIDIObjectGetStringProperty(src, kMIDIPropertyName, &name);
+  MIDIObjectGetIntegerProperty(src, kMIDIPropertyUniqueID, &sourceID);
+
+  /* Create a client and a port */
+  MIDIClientCreate(CFSTR("music-keyboard"), NULL, NULL, &client);
+  MIDIInputPortCreate(client, CFSTR("music-keyboard port"), readPackets, NULL, &port);
+
+  /* Link the port with the endpoint */
+  MIDIPortConnectSource(port, src, &state);
+
+  /* Print instructions */
+  fprintf(stderr, "MIDI source : %s (%d)\n", CFStringGetCStringPtr(name, kCFStringEncodingUTF8), sourceID);
+  fprintf(stderr, "Press sustain button while recording.\n\n");
+
+  while (state.running) {
+    sleep(1);
+  }
+}
